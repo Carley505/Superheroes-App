4ed73137e5f792ce50934a1ab96589f5670e2096
Carley505 <callistusngeywa@gmail.com>
2026-05-23 14:14:35 +0300

    Improve on layout and image sizing

--- app/src/main/java/com/example/superheroesapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroesapp/MainActivity.kt
@@ -6,29 +6,40 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superheroesapp.data.DataSource
@@ -53,11 +64,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HeroApp() {
-    HeroList(DataSource().loadHeroes())
+    Scaffold(
+        topBar = {
+            SuperHeroesTopAppBar()
+        }
+    ) {it->
+        HeroList(DataSource().loadHeroes(), contentPadding = it)
+    }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HeroList(heroes: List<Hero>){
+fun SuperHeroesTopAppBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        }
+    )
+}
+
+@Composable
+fun HeroList(heroes: List<Hero>, contentPadding: PaddingValues){
     LazyColumn(
+        contentPadding = contentPadding,
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier.padding(16.dp)
     ) {
@@ -69,23 +101,33 @@ fun HeroList(heroes: List<Hero>){
 
 @Composable
 fun HeroCard(hero: Hero, modifier: Modifier = Modifier) {
-    Card(modifier = modifier.fillMaxWidth().clip(MaterialTheme.shapes.small)) {
-        Row(modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))) {
-            CardLeft(title = hero.title, description = hero.description)
-            Spacer(modifier = Modifier.weight(1f))
+    Card(
+        shape = MaterialTheme.shapes.small,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)).sizeIn(minHeight = 72.dp)
+        ) {
+            CardLeft(title = hero.title, description = hero.description, modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.width(dimensionResource(R.dimen.padding_small)))
             CardRight(image = hero.image, title = hero.title)
         }
     }
 }
 
 @Composable
-fun CardLeft(title: Int, description: Int) {
-    Column() {
+fun CardLeft(title: Int, description: Int, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
         Text(
-            text = stringResource(title)
+            text = stringResource(title),
+            style = MaterialTheme.typography.labelLarge
         )
         Text(
-            text = stringResource(description)
+            text = stringResource(description),
+            style = MaterialTheme.typography.labelMedium,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
         )
     }
 }
@@ -95,7 +137,8 @@ fun CardRight(image: Int, title: Int) {
     Image(
         painter = painterResource(image),
         contentDescription = stringResource(title),
-        modifier = Modifier.size(dimensionResource(R.dimen.padding_large)).clip(MaterialTheme.shapes.small)
+        contentScale = ContentScale.FillBounds,
+        modifier = Modifier.size(dimensionResource(R.dimen.image_size)).clip(MaterialTheme.shapes.small)
     )
 }
 
@@ -105,5 +148,7 @@ fun CardRight(image: Int, title: Int) {
 )
 @Composable
 fun HeroAppPreview() {
-    HeroApp()
+    SuperheroesAppTheme() {
+        HeroApp()
+    }
 }
--- app/src/main/java/com/example/superheroesapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/superheroesapp/ui/theme/Type.kt
@@ -2,12 +2,33 @@ package com.example.superheroesapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.superheroesapp.R
+
+
+val Cabin = FontFamily(
+    Font(R.font.cabin_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
+        fontSize = 32.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Cabin,
+        fontSize = 16.sp
+    ),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
